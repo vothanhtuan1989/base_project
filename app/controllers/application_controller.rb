beb8d544a448b0fc48fd4339cde6faf9b8d70177
Tuan Vo <vothanhtuan1989@gmail.com>
2024-05-17 21:47:37 +0700

    add devise gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
+# This is the main controller of the application.
+# It is responsible for authenticating the user before any action is performed.
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
 end
